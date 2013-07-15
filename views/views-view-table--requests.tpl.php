1591481a3c60fa3386a5755a90d2493b80086f58
emav <emanuele.quinto@gmail.com>
2013-07-15 18:27:49 +0200

    FIX request view HTML for OA-3527 (2)

--- views/views-view-table--requests.tpl.php
+++ views/views-view-table--requests.tpl.php
@@ -59,45 +59,47 @@ $field_request_referrals = implode($referral_list, '<br/>');
 
 
 ?>
-<h2 property="dc:title" datatype="" class="node-title"><?php print $title; ?></h2>
-<?php if ($field_emergencies): ?>
-<div class="field field-name-field-emergencies field-type-taxonomy-term-reference field-label-inline clearfix">
-  <div class="field-label"><?php print t('Emergencies');?>:&nbsp;</div>
-  <div class="field-items"><div class="field-item"><?php print $field_emergencies; ?></div></div>
-</div>
-<?php endif; ?>
-<div class="field field-name-field-request-audience field-type-list field-label-inline clearfix">
-  <div class="field-label"><?php print t('Audience');?>:&nbsp;</div>
-  <div class="field-items"><div class="field-item"><?php print $field_request_audience; ?></div></div>
-</div>
-<div class="field field-name-field-request-deadline field-type-datetime field-label-inline clearfix">
-  <div class="field-label"><?php print t('Deadline');?>:&nbsp;</div>
-  <div class="field-items"><div class="field-item"><?php print $field_request_deadline; ?></div></div>
-</div>
-<div class="field field-name-field-referrals field-type-list field-label-inline clearfix">
-  <div class="field-label"><?php print t('Requested by');?>:&nbsp;</div>
-  <div class="field-items"><div class="field-item"><?php print $field_request_referrals;?></div></div>
-</div>
-<table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
-  <caption></caption>
-  <thead>
-    <tr>
-      <?php foreach ($header as $field => $label): ?>
-        <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
-          <?php print $label; ?>
-        </th>
-      <?php endforeach; ?>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($rows as $row_count => $row): ?>
-      <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
-        <?php foreach ($row as $field => $content): ?>
-          <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
-            <?php print $content; ?>
-          </td>
+<div class="views-row">
+  <h2 property="dc:title" datatype="" class="node-title"><?php print $title; ?></h2>
+  <?php if ($field_emergencies): ?>
+  <div class="field field-name-field-emergencies field-type-taxonomy-term-reference field-label-inline clearfix">
+    <div class="field-label"><?php print t('Emergencies');?>:&nbsp;</div>
+    <div class="field-items"><div class="field-item"><?php print $field_emergencies; ?></div></div>
+  </div>
+  <?php endif; ?>
+  <div class="field field-name-field-request-audience field-type-list field-label-inline clearfix">
+    <div class="field-label"><?php print t('Audience');?>:&nbsp;</div>
+    <div class="field-items"><div class="field-item"><?php print $field_request_audience; ?></div></div>
+  </div>
+  <div class="field field-name-field-request-deadline field-type-datetime field-label-inline clearfix">
+    <div class="field-label"><?php print t('Deadline');?>:&nbsp;</div>
+    <div class="field-items"><div class="field-item"><?php print $field_request_deadline; ?></div></div>
+  </div>
+  <div class="field field-name-field-referrals field-type-list field-label-inline clearfix">
+    <div class="field-label"><?php print t('Requested by');?>:&nbsp;</div>
+    <div class="field-items"><div class="field-item"><?php print $field_request_referrals;?></div></div>
+  </div>
+  <table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
+    <caption></caption>
+    <thead>
+      <tr>
+        <?php foreach ($header as $field => $label): ?>
+          <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
+            <?php print $label; ?>
+          </th>
         <?php endforeach; ?>
       </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      <?php foreach ($rows as $row_count => $row): ?>
+        <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
+          <?php foreach ($row as $field => $content): ?>
+            <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+              <?php print $content; ?>
+            </td>
+          <?php endforeach; ?>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+</div>
